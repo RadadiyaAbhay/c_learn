0c60acee0ab7b5d5b2ea88f5b5a8003022760b27
RadadiyaAbhay <abhay592004@gmail.com>
2023-03-22 17:53:03 +0530

    task_44

--- Day 9 _PR 2/task_4_2.c
+++ Day 9 _PR 2/task_4_2.c
@@ -1,39 +1,42 @@
 #include<stdio.h>
- main(){
+main(){
 	
-	char a ;
+    char a[2];
 	
 	printf("Enter character of Day Name = ");
 	scanf("%s",&a);
 	
-	
-	switch(a){
-		case 'S':
-			printf("Sunday");
-			break;
-		
+	switch(a[0]){
 		case 'M':
 			printf("Monday");
 			break;
 		
 		case 'T':
+			if(a[1] == 'u'){
 			printf("Tuesday");
+			}else if (a[1] == 'h'){
+			printf("Thursday");
+			}else{
+			printf("Enter vaild First character of Day Name in capital");	
+			}
 			break;
 		
 		case 'W':
 			printf("Wednesday");
 			break;
-		
-		case 'TH':
-			printf("Thursday");
-			break;
-		
+
 		case 'F':
 			printf("Friday");
 			break;
 			
-		case 'SA':
+		case 'S':
+			if(a[1] == 'a'){
 			printf("Saturday");
+			}else if (a[1] == 'u'){
+			printf("Sunday");
+			}else{
+			printf("Enter vaild First character of Day Name in capital");	
+			}
 			break;
 		
 		default:
